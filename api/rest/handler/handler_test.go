69aea6f235312c7995a74bd4d98f5c9f3751ee87
Ruslan Frolov <Ezpath@yandex.ru>
2022-02-06 17:58:29 +0300

    add dummy test

---
+++ api/rest/handler/handler_test.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	tests := []struct {
+		name string
+		a int
+		b int
+		want int
+	}{
+		{
+			name: "dummy sum",
+			a: 1,
+			b: 2,
+			want: 3,
+		},
+		{
+			name: "negative num",
+			a: 1,
+			b: -1,
+			want: 0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.a + tt.b
+			if got != tt.want {
+				t.Error("Sum test failed")
+			}
+		})
+	}
+}
