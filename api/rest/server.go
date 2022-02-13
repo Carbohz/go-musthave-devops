1049aebe77c88c5ffdb7eec6caf44caf2473814a
Ruslan Frolov <Ezpath@yandex.ru>
2022-02-13 12:52:00 +0300

    fix go vet test error

--- api/rest/server.go
+++ api/rest/server.go
@@ -33,32 +33,29 @@ func NewAPIServer(config server.Config, serverSvc server.Processor) (*APIServer,
 
 func (s *APIServer) Run(ctx context.Context) error {
 
+	//go func() {
+	//	storeTicker := time.NewTicker(s.config.StoreInterval)
+	//	defer storeTicker.Stop()
+	//	for {
+	//		select {
+	//		case <-storeTicker.C:
+	//			s.serverSvc.Dump()
+	//		//case <-ctx.Done():
+	//		//	log.Println("Dumping and exiting")
+	//		//	s.serverSvc.Dump()
+	//		//	return
+	//		}
+	//	}
+	//}()
+
 	go func() {
 		storeTicker := time.NewTicker(s.config.StoreInterval)
 		defer storeTicker.Stop()
 		for {
-			select {
-			case <-storeTicker.C:
-				s.serverSvc.Dump()
-			//case <-ctx.Done():
-			//	log.Println("Dumping and exiting")
-			//	s.serverSvc.Dump()
-			//	return
-			}
+			<-storeTicker.C
+			s.serverSvc.Dump()
 		}
 	}()
-
-	//storeTicker := time.NewTicker(s.config.StoreInterval)
-	//defer storeTicker.Stop()
-	//for {
-	//	select {
-	//	case <-storeTicker.C:
-	//		s.serverSvc.Dump()
-	//	case <-ctx.Done():
-	//		log.Println("Dumping and exiting")
-	//		s.serverSvc.Dump()
-	//	}
-	//}
 
 	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		return fmt.Errorf("listen: %s", err)
