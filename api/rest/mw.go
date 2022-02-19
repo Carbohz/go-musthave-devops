e59dc39a74ebcb7aacb91c39f261520b19eaa0af
Ruslan Frolov <Ezpath@yandex.ru>
2022-02-19 11:47:19 +0300

    change Sprintf to Sprint

--- api/rest/mw.go
+++ api/rest/mw.go
@@ -7,30 +7,6 @@ import (
 	"net/http"
 )
 
-func updateURLValidator(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		metricType := chi.URLParam(r, "metricType")
-		if err := model.ValidateType(metricType); err != nil {
-			reason := fmt.Sprintf("Invalid url request: unknown metric type %s", metricType)
-			http.Error(w, reason, http.StatusNotImplemented)
-			return
-		}
-
-		if metricName := chi.URLParam(r, "metricName"); metricName == "" {
-			reason := fmt.Sprintf("Invalid url request: missing /{metric_name}/{metric_value} in the end of url")
-			http.Error(w, reason, http.StatusNotFound)
-		}
-
-		if metricValue := chi.URLParam(r, "metricValue"); metricValue == "" {
-			reason := fmt.Sprintf("Invalid url request: missing /{metric_value} in the end of url")
-			http.Error(w, reason, http.StatusNotFound)
-		}
-
-		next.ServeHTTP(w, r)
-	}
-	return http.HandlerFunc(fn)
-}
-
 func metricTypeValidator(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r * http.Request) {
 		metricType := chi.URLParam(r, "metricType")
@@ -48,7 +24,7 @@ func metricTypeValidator(next http.Handler) http.Handler {
 func metricNameValidator(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r * http.Request) {
 		if metricName := chi.URLParam(r, "metricName"); metricName == "" {
-			reason := fmt.Sprintf("Invalid url request: empty metric name. Maybe missing /{metric_name}/{metric_value} in the end of url?")
+			reason := fmt.Sprint("Invalid url request: empty metric name. Maybe missing /{metric_name}/{metric_value} in the end of url?")
 			http.Error(w, reason, http.StatusNotFound)
 		}
 
@@ -60,7 +36,7 @@ func metricNameValidator(next http.Handler) http.Handler {
 func metricValueValidator(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r * http.Request) {
 		if metricValue := chi.URLParam(r, "metricValue"); metricValue == "" {
-			reason := fmt.Sprintf("Invalid url request: empty metric value. Maybe missing /{metric_value} in the end of url?")
+			reason := fmt.Sprint("Invalid url request: empty metric value. Maybe missing /{metric_value} in the end of url?")
 			http.Error(w, reason, http.StatusNotFound)
 		}
 
